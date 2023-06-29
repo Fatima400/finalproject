650ebf3b89333a212f7bb122983250e39b73410d
Fatima400 <fkhalil440@gmail.com>
2023-06-30 00:02:21 +0200

    add function for calories and BMR

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -43,27 +43,38 @@ const RecipeComponent = () => {
     }
   };
 
+  const calorieDifference = bmiValue - totalCalories;
+  const isCalorieExceeded = calorieDifference < 0;
+  let message = "";
+  let messageColor = "";
+  if (isCalorieExceeded) {
+    message = "Calorie intake exceeds BMR. Consider adjusting your diet.";
+    messageColor = "red";
+  } else {
+    message = "Calorie intake is within BMR.";
+    messageColor = "green";
+  }
+
   return (
     <>
       <HamburgerMeun />
       <p className="call">Total Calories: {totalCalories}</p>
-      <p className="call">Calorie Difference: {bmiValue - totalCalories}</p>
+      <p className="call">Calorie Difference: {calorieDifference}</p>
+      <p  className='textm' style={{ color: messageColor }}>{message}</p>
       <div className="con">
         {recipes.map((recipe, index) => (
           <div key={index} className="book">
             <p className="ing">{recipe.ingredients}</p>
             <button className="cll" onClick={() => selectRecipe(recipe)}>Add to Basket</button>
-              <button className="cll" onClick={() => removeRecipe(recipe)}>Remove from Basket</button>
+            <button className="cll" onClick={() => removeRecipe(recipe)}>Remove from Basket</button>
             <div className="cover">
               <img src={recipe.photo} alt={recipe.name} className="imgg" />
               <p>{recipe.name}</p>
               <p>Calories: {recipe.calories}</p>
-             
             </div>
           </div>
         ))}
       </div>
-   
       <Footer />
     </>
   );
